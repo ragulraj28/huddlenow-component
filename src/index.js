857f12cb3b2e7726dfc268393ed834314e65ab6a
ragulraj28 <rraj9350@gmail.com>
2023-08-03 19:35:40 +0530

    github page deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/main.scss';
+import './style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
